a8346a101d0e5116062c6f64b627acd059503a54
hunan <544533702@qq.com>
2021-06-09 11:02:12 +0800

    ddsds

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 Vue.config.productionTip = false;
-var sdjkds;
-var sdjskjksdlk;
+var sdj;
 new Vue({
   render: (hello) => hello(App),
 }).$mount('#app');
